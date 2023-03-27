cb160ba26714bbbbcf1c306f1df325252bf5d226
amibening <amibening@hotmail.com>
2023-03-27 20:22:52 +0000

    More code questions and egs

--- mysandbox/Irfanquestions.hs
+++ mysandbox/Irfanquestions.hs
@@ -1,5 +1,7 @@
 module Irfanquestions where
 
+import Control.Monad (when)
+
 {-
 data Pokemon = MkPokemon Name Id [Power]
     deriving Show
@@ -14,7 +16,7 @@ fst' ((w,_) , _) = W
 ----getName :: Pokemon -> Name
 getName (MkPokemon name _ _) = name
 
--- getPower and fifth solved
+-- getPower and fifth solutions provided
 
 getPower (MkPokemon _ _ (p : _)) = p
 
@@ -23,6 +25,13 @@ fifth (_ : (_ : (_ : (_ : (x : _))))) = x
 -}
 
 -- Irfan questions 26th March 2023
+
+--getFifthChar :: [a] -> a
+--getFifthChar (_ : _ : _ : _ : x : _) = x
+
+--getFifthChar _ = Nothing
+
+-- Question 2
 -- define types
 type Name = String
 
@@ -43,3 +52,26 @@ getAllPowers (MkPokemon _ _ p) = p
 
 -- get first power
 getFirstPower (MkPokemon _ _ (p : _)) = p
+
+-- Question 1
+
+--type a = String
+
+myFunction :: Int -> Int -> Int
+myFunction x y = myVar1
+  where
+    myVar1 :: Int
+    myVar1 = 1
+
+--
+
+--type a = Char
+fifth :: [a] -> a
+--fifth (_ : _ : _ : _ : x : _) = x
+
+--getFifthChar :: [b] -> b
+--getFifthChar (_ : _ : _ : _ : y : _) = y
+
+fifth (_ : (_ : (_ : (_ : (x : _))))) = x
+
+--fst' :: ((w, _), _) = w
