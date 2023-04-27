ecccf60c599c3f87e7c461bdd7927e4618f8a765
amibening <amibening@hotmail.com>
2023-04-27 15:31:42 +0000

    Tidy up A2 and highlight issue for latz

--- solutions/A2.hs
+++ solutions/A2.hs
@@ -9,21 +9,14 @@ import Data.List (intercalate)
 -- *** Assignment 2-1 *** --
 
 -- Q#01
-
---promptPlayer :: Player -> String
---promptPlayer player = concat ["Player ", showSquare p ", - enter row and column: "]
+promptPlayer :: Player -> String
+promptPlayer p = concat ["Player ", showSquare p, ", enter row and column: "]
 
 -- Q#02
-
---_SIZE1_ :: Int
---_SIZE1_ = 4
-
 _RANGE_ :: [Int]
---_RANGE_ = [0 _ ((_SIZE_) -1)]
 _RANGE_ = [0 .. _SIZE_ - 1]
 
 -- Q#03
-
 -- Function to check if a character is a digit -  i.e. isDigit '1'
 isDigit :: Char -> Bool
 isDigit c = c `elem` ['0' .. '9']
@@ -35,27 +28,19 @@ readDigit c
   | otherwise = -1 -- Return -1 for non-digit characters
 
 -- Q#04
-
--- CODE THAT IS IN A1 FILE
--- Define the Square type with three constructors
---data Square = X | O | Empty deriving (Show)
--- Define the size constant
---_SIZE_ :: Int
---_SIZE_ = 3
-
 -- Declare _EMPTY_ROW_ and _EMPTY_BOARD_ using replicate and Empty constructor
-_EMPTY_ROW_ :: [Square]
+-- Unsure why this is an issue when run with elem... as in question 4!!
+_EMPTY_ROW_ :: Row
 _EMPTY_ROW_ = replicate _SIZE_ Empty
 
-_EMPTY_BOARD_ :: [[Square]]
+_EMPTY_BOARD_ :: Board
 _EMPTY_BOARD_ = replicate _SIZE_ _EMPTY_ROW_
 
 -- Q#05
-
--- Define the Square type with three constructors
---data Square = X | O | Empty deriving (Show)
-
 -- Define a function to check if a Square is Empty
+
+-- ** Why an issue when running !!??
+
 isEmpty :: Square -> Bool
 isEmpty Empty = True
 isEmpty _ = False
@@ -64,7 +49,12 @@ isEmpty _ = False
 isTied :: Board -> Bool
 isTied board = and $ map (not . any isEmpty) board
 
+-- isTied _EMPTY_BOARD_ (works ok)
+-- isTied _TIED_BOARD_ (errors why??)
 -- Declare a constant _TIED_BOARD_ to test the isTied function
+
+-- ** Why an issue when using !!??
+
 _TIED_BOARD_ :: Board
 _TIED_BOARD_ =
   [ [X, O, O],
@@ -72,39 +62,25 @@ _TIED_BOARD_ =
     [O, X, O]
   ]
 
---isTied = undefined
-
---_TIED_BOARD_ = undefined
-
 -- Q#06
-
---indexRowStrings = undefined
-
 indexRowStrings :: [String] -> [(Char, String)]
 indexRowStrings strs = zip ['A' ..] strs
 
 -- Q#07
-
---formatLine = undefined
-
 formatLine :: [String] -> String
 formatLine strs = "_|" ++ intercalate "|_" strs ++ "|_"
 
 -- *** Assignment 2-2 *** --
 
 -- Q#08
-
---isMoveInBounds = undefined
-
-isMoveInBounds :: (Int, Int) -> Bool
-isMoveInBounds (row, col) = all (\x -> x >= 0 && x < _SIZE_) [row, col]
-
--- NOTES on my answer Q8 using Lamda
+--NOTES on my answer Q8 using Lamda
 --In Haskell, the code isMoveInBounds is defining a function that takes a tuple of two Int values, (row, col), and returns a Bool value indicating whether the given position is within the bounds of a square board of size _SIZE_.
 --The all function is used to check that both row and col are greater than or equal to 0 and less than _SIZE_. The expression \x -> x >= 0 && x < _SIZE_ is a lambda function that takes a single argument x and returns a Boolean value indicating whether x is within the bounds of the board. The all function applies this lambda function to each element of the list [row, col] and returns True only if the lambda function returns True for all elements.
 --Therefore, if _SIZE_ = 3, the function isMoveInBounds will return True if the position (row, col) is within the bounds of a 3x3 square board, and False otherwise.
+isMoveInBounds :: (Int, Int) -> Bool
+isMoveInBounds (row, col) = all (\x -> x >= 0 && x < _SIZE_) [row, col]
 
--- suggested ans
+-- Ian's ans as in video
 -- type Move = (Int, Int)
 -- isMoveInBounds :: Move -> Bool
 -- isMoveInBounds (row, col) = and [ row >= 0, r < _SIZE_, c >= 0, c < _SIZE_]
